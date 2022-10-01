df62e837cfa531fd53cfdce621d44c2e43a093a8
Willie Johnson <liwa.johnson@gmail.com>
2022-10-01 08:14:05 -0400

    Initial Commit

---
+++ RAM/RAMApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAMApp.swift
+//  RAM
+//
+//  Created by Willie Liwa Johnson on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct RAMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
